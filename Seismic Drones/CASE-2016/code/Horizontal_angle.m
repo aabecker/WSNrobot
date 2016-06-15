03c1ea69ef74495829ebd51500c8c365a2956727
srikanthkvs11 <skvenkatasudarshan@uh.edu>
2016-06-15 10:28:25 -0500

    Added a folder for Reviews with the necessary word document

--- Seismic Drones/CASE-2016/code/Horizontal_angle.m
+++ Seismic Drones/CASE-2016/code/Horizontal_angle.m
@@ -14,7 +14,7 @@ X1 =ones(length(x1),1);
 for i =1:length(x1)
     X1(i,1) = AngHorizontal(x1(i,1),x1(i,2));
 end
-
+X1 =abs(X1);
 x2=[-1,0;
     -2,2;
     0,2;
@@ -29,7 +29,7 @@ X2 =ones(length(x2),1);
 for i =1:length(x2)
     X2(i,1) = AngHorizontal(x2(i,1),x2(i,2));
 end
-
+X2 =abs(X2);
 x3=[-1,1;
     -3,-4;
     0,-5;
@@ -45,5 +45,5 @@ X3 =ones(length(x3),1);
 for i =1:length(x3)
     X3(i,1) = AngHorizontal(x3(i,1),x3(i,2));
 end
-
+X3 =abs(X3);
 boxplot([X1,X2,X3],'plotstyle','traditional','boxstyle','outline','colors','rgb','labels',{'sand','grass','dry clay'})
